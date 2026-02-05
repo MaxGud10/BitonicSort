2cb1f39f308784ba4dd094bcfa6589d62b2bb463
Max <karatemaksim0110@gmail.com>
2026-02-05 15:53:51 +0300

    немного изменил OnCPU и добавил оптимизацию на OnGPU

--- OnCPU/include/bitonic_sort.h
+++ OnCPU/include/bitonic_sort.h
@@ -1,35 +1,38 @@
 #pragma once
 
-#include <vector>
-#include <algorithm>
 #include <iostream>
+#include <stack>
+#include <iterator>
+#include <utility>
+#include <algorithm>
+#include "utils.h"
 
 namespace bitonic
 {
-	template <typename RandomIt>
-	void Sort(RandomIt begin, RandomIt end)
+
+template <typename C>
+void Merge(C& arr, size_t lhs_id, size_t block_size, bool ascending)
+{
+    for (size_t k = block_size / 2; k > 0; k /= 2)
+		for (size_t i = lhs_id; i + k < lhs_id + block_size; ++i)
+            if ((arr[i] > arr[i + k]) == ascending)
+				std::swap(arr[i], arr[i + k]);
+}
+
+template <typename C>
+void Sort(C& data)
+{
+    size_t data_size = data.size();
+    for (size_t block_size = 2; block_size <= data_size; block_size *= 2)
 	{
-		size_t data_size = std::distance(begin, end);
-		// std::sort(begin, end);
-
-		if (data_size <= 1) return;
-
-
-		for (size_t i = 0; i < data_size / 2; ++i)
+        for (size_t lhs_id = 0; lhs_id < data_size; lhs_id += block_size)
 		{
-			auto lhs_iter = std::next(begin, i);
-			auto rhs_iter = std::next(begin, i + data_size / 2);
+            bool is_ascending = ((lhs_id / block_size) % 2 == 0);
+            Merge(data, lhs_id, block_size, is_ascending);
+        }
 
-			if (*lhs_iter > *rhs_iter)
-			{
-				std::cout << "swapping " << *lhs_iter << " and " << *rhs_iter << '\n';
-				std::iter_swap(lhs_iter, rhs_iter);
-			}
-		}
+		utils::hight_dump(data);
+    }
+}
 
-		auto mid = std::next(begin, data_size / 2);
-
-		Sort(begin, mid);
-		Sort(mid, end);
-	}
-};
+}; // bitonic
